81f16039a9f1111e83a3243582b060cc7f197b2a
Sunny <ssharda92@gmail.com>
2017-09-07 15:30:23 +0100

    about to use mvc model

--- week4/Web server/server.js
+++ week4/Web server/server.js
@@ -2,44 +2,54 @@ const http = require('http');
 const fs = require('fs');
 const handles = require("../BE-first_web_server/tweetinfo/handles")
 const path = require("path")
-
+const infoPath = "../BE-first_web_server/tweetinfo"
 const url = require("url")
-// const handles = [
-//   'mauro',
-//   'dennis',
-//   'francis'
-// ]
+
 var userdata = "../BE-first_web_server/tweetinfo/users/"
 const server = http.createServer(function (request, response) {
-    console.log(request.method);
-    console.log(request.url);
-
+    //console.log(request.method);
+    //console.log(request.url);
+var queryArr = ["count"]
     if (request.method === 'GET' && request.url === '/handles') {
         response.setHeader('Content-Type', 'application/json');
         response.write(JSON.stringify(handles));
         response.end();
     }
-    if (request.method === 'GET' && request.url.substring(0, 5) === '/user') {
-        let userHandle = request.url.substring(5)
-        let requireHandle = fs.readFile("../BE-first_web_server/tweetinfo/users" + userHandle + "tweetdata.json", function (err, data) {
-            if (err) response.write("not found");
+    else if (request.method === 'GET' && request.url.substring(0, 5) === '/user') {
+        let userHandle = request.url.substring(5).split("?")[0]
+        let queries = request.url.split("?").slice(1).map(query => query.split("="))
+        
+        let requireHandle = fs.readFile(path.join(infoPath, "users", userHandle + "tweetdata.json"), function (err, data) {
+            if (err) response.statusCode = 404; response.end()
+            
             response.setHeader('Content-Type', 'application/json');
             response.write(data.toString());
             response.end();
         })
-
-
-
     }
-    else if (request.method === 'GET' && request.url.substring(0, 7) === '/tweets') {
-        let userHandle = request.url.substring(7)
-        let requireHandle = fs.readFile("../BE-first_web_server/tweetinfo/tweets" + userHandle + "tweetdata.json", function (err, data) {
-            if (err) response.write("not found")
-            response.setHeader('Content-Type', 'application/json');
-            response.write(data.toString());
-            response.end();
-        })
 
+    else if (request.method === 'GET' && request.url.substring(0, 7) === '/tweets') {
+        console.log(request.url);
+        let userHandle = request.url.substring(7).split("?")[0]
+        console.log(userHandle);
+        let queries = request.url.split("?").slice(1).map(query => query.split("="))
+        console.log(queries);
+        let requireHandle = fs.readFile(path.join(infoPath, "tweets", userHandle + "tweetdata.json"), function (err, data) {
+            if (err) response.statusCode = 404; response.end();
+            response.setHeader('Content-Type', 'application/json');
+            let parseData = JSON.parse(data.toString())
+            // response.write(JSON.parse(data.toString()));
+            let slicedData = parseData
+            queries.forEach(function(query){
+                if (!queryArr.includes(query[0])) {response.statusCode = 400
+                    response.end();}
+                if (query[0] === "count") {
+                 slicedData = parseData.slice(0,query[1])
+                }
+            });
+            response.write(JSON.stringify(slicedData));
+            response.end();
+        });
     }
 
     else {
@@ -52,10 +62,6 @@ const server = http.createServer(function (request, response) {
         response.write(JSON.stringify(body));
         response.end();
     }
-    if (request.method === 'GET' && request.url === '/handles') {
-        response.setHeader('Content-Type', 'application/json');
-        response.write(JSON.stringify(handles));
-        response.end();
-    };
+
 })
 server.listen(3000);
